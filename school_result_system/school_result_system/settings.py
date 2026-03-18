e14f38a9dc18f0cfd1bc50563847830b2170389b
MALIKB214 <adebayomalik214@gmail.com>
2026-03-18 03:08:25 +0100

    Updated

--- school_result_system/school_result_system/settings.py
+++ school_result_system/school_result_system/settings.py
@@ -62,7 +62,7 @@ INSTALLED_APPS = [
     'django_extensions',
     'rest_framework',
     'django_celery_results',
-    # 'django_celery_beat',
+    # pip'django_celery_beat',
     'accounts',
     'students',
     'academics',
